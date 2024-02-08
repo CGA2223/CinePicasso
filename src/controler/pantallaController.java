066a8555c23ba0f70a1173b877cd7494042bc830
alvarodiaz12 <115473484+alvarodiaz12@users.noreply.github.com>
2024-02-08 12:44:01 +0100

    javadoc
    javadoc

--- src/controler/pantallaController.java
+++ src/controler/pantallaController.java
@@ -10,12 +10,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import models.Usuarios;
 import utiles.ListaUsuario;
 
+/**
+ * La clase pantallaController controla la lógica de la ventana de registro de nuevos usuarios.
+ * Administra la interacción del usuario con la interfaz gráfica de registro.
+ */
 public class pantallaController {
 
     @FXML
@@ -27,11 +32,15 @@ public class pantallaController {
     @FXML
     private ComboBox<String> cboxGusto;
 
-	public void setCboxGusto(ArrayList<String> cboxGusto) {
-		this.cboxGusto.setItems(FXCollections.observableArrayList(cboxGusto));
-	}
+    /**
+     * Método para configurar las opciones del ComboBox de gustos de películas.
+     * @param cboxGusto Lista de opciones a configurar en el ComboBox.
+     */
+    public void setCboxGusto(ArrayList<String> cboxGusto) {
+        this.cboxGusto.setItems(FXCollections.observableArrayList(cboxGusto));
+    }
 
-	@FXML
+    @FXML
     private ImageView imageDecoracionInferior2;
 
     @FXML
@@ -67,22 +76,28 @@ public class pantallaController {
     @FXML
     private TextField txtNombre;
 
+    /**
+     * Método llamado cuando el usuario desea añadir un nuevo usuario a la lista.
+     * Verifica si las contraseñas coinciden y crea un nuevo usuario si es así.
+     * @param event El evento que desencadena este método (presionar el botón de añadir usuario).
+     */
     @FXML
     void anadirUsuarioALaLista(ActionEvent event) {
-    	
-    	if(txtContrasena.getText().equals(txtConfirmarContrasena.getText())) {
-        	Usuarios usuario = ListaUsuario.creaUser(txtNombre.getText(), Date.valueOf(txtFechaDeNacimiento.getText()) , txtGenero.getText(),
-        			txtCorreo.getText(), txtContrasena.getText(), (String) cboxGusto.getValue());
-        	ListaUsuario.addUser(usuario);
-    	} else {
-    		System.out.println("Error: La contraseña no es igual a confirmar contraseña");
-    	}
-
+        if(txtContrasena.getText().equals(txtConfirmarContrasena.getText())) {
+            Usuarios usuario = ListaUsuario.creaUser(txtNombre.getText(), Date.valueOf(txtFechaDeNacimiento.getText()) , txtGenero.getText(),
+                    txtCorreo.getText(), txtContrasena.getText(), (String) cboxGusto.getValue());
+            ListaUsuario.addUser(usuario);
+        } else {
+            System.out.println("Error: La contraseña no es igual a confirmar contraseña");
+        }
     }
 
+    /**
+     * Método llamado cuando el usuario desea volver a la ventana de inicio de sesión.
+     * @param event El evento que desencadena este método (presionar el botón de volver al login).
+     */
     @FXML
     void volverALoggin(ActionEvent event) {
-    	LoginController.volverALoggin();
+        LoginController.volverALoggin();
     }
-
 }
